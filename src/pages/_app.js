d8f2fdffed109bc67de08ff975f96323887503e8
Brian <bdlowery2@gmail.com>
2023-02-28 02:55:47 -0700

    initial commit

---
+++ src/pages/_app.js
@@ -0,0 +1,9 @@
+import "@/styles/globals.css";
+
+// global layout?
+// Component prop is the active page
+// pageProps has initial props that were preloaded for the page
+
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} />;
+}
